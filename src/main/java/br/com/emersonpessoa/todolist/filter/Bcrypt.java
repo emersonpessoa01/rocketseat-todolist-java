9e45e4498bae6e6002f94f5752411e8bac2e13af
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-10-12 21:12:41 -0300

    upload aula03 :rocket:

---
+++ src/main/java/br/com/emersonpessoa/todolist/filter/Bcrypt.java
@@ -0,0 +1,9 @@
+package br.com.emersonpessoa.todolist.filter;
+
+public class Bcrypt {
+
+    public static Object verifyer() {
+        return null;
+    }
+
+}
---
+++ src/main/java/br/com/emersonpessoa/todolist/task/TaskModel.java
@@ -0,0 +1,45 @@
+package br.com.emersonpessoa.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+/**
+ * ID
+ * Usuário
+ * Descrição
+ * Título
+ * Data de Início
+ * Data de término
+ * Prioridade
+ */
+@Data
+@Entity(name="tb_tasks")
+public class TaskModel {
+    
+    @Id //para apontar como primary key
+    @GeneratedValue(generator = "UUID")// Gerar chave primária de forma automática
+    private UUID id;
+    private String description;
+
+    
+    @Column(length=50)//Limitar a quantidade de caracteres que seja possível p/ colocar no title
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
+}
