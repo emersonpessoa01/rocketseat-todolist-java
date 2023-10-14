5f3cf80d6dc533ce9c7e521cb75133741bfe863b
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-10-14 15:30:47 -0300

    finished aula04 :rocket:

---
+++ src/main/java/br/com/emersonpessoa/todolist/utils/Utils.java
@@ -0,0 +1,38 @@
+package br.com.emersonpessoa.todolist.utils;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class Utils {
+
+    // Copiar tudo que não nulo
+    public static void copyNonNullProperties(Object source, Object target) {
+        BeanUtils.copyProperties(source, target, getNullPropertyNames(source));
+    }
+
+    // Vai pegar tudo de propriedade nula nos objetos
+    public static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+
+        for (PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+
+            if (srcValue == null) {
+                emptyNames.add(pd.getName());
+            }
+        }
+
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+
+    }
+
+}
